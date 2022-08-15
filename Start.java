326caf64581f07772d0597e7629cc0602b610fad
chakkrapan3489 <110364536+chakkrapan3489@users.noreply.github.com>
2022-08-15 16:41:28 +0700

    Update Start.java

--- Start.java
+++ Start.java
@@ -3,6 +3,7 @@ class Start
     public static void main(String[] args) {
         System.out.println("bank");
         System.out.println("pipe");
+        System.out.println("PaPa");
         
     }
 
