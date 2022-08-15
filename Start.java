10f9bf6bdfb0fbbc89df74ce228e9f86ac6d8039
Lapvisuthisaroj <chakkrapan.lapvisuthisaroj@allianz.com>
2022-08-15 16:53:17 +0700

    haha

--- Start.java
+++ Start.java
@@ -3,10 +3,13 @@ class Start
     public static void main(String[] args) {
         System.out.println("bank");
         System.out.println("pipe");
-        System.out.println("Mama");
-        
+        System.out.println("Mama"); 
     }
 
+    public static int Add(int a,int b)
+    {
+        return a+b;
+    }
     
    
 }
